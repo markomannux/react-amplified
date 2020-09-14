41b97f975cacb6b585422dcc2ab57d45029222b8
Marco <marco.mannucci@hey.com>
2020-09-14 15:27:03 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify, { API } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
+Amplify.configure(awsconfig);
+
 function App() {
+  const [message, setMessage] = React.useState("");
+  const [apiData, setApiData] = React.useState("")
+
+  const handleClick = async () => {
+    const data = await API.get("echoapi", "/echo", {
+      queryStringParameters: {
+        message
+      }
+    });
+    setApiData(data.success);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <p>Welcome! My favorite quote is: {apiData}</p>
+        <input type="text" value={message} onChange={event => setMessage(event.target.value)} />
+        <button onClick={handleClick}>Click me!</button>
+        <AmplifySignOut />
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
